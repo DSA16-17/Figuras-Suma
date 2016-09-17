ebd0250aeaa1e4e2ef04cf19e81d1afcf988b76b
alberto antoñanzas acero <alberto antoanzas acero>
2016-09-17 12:29:11 +0200

    Initial commit

---
+++ src/Figuras.java
@@ -0,0 +1,44 @@
+package proyecto;
+/**
+ * Created by alber on 17/09/2016.
+ */
+public interface Figuras {
+    public double area();
+
+}
+class rectangulo implements Figuras{
+    double l1, l2;
+    public rectangulo(double l1, double l2) {
+        this.l1=l1;
+        this.l2=l2;
+    }
+    public double area(){
+        return this.l1*this.l2;
+    }
+}
+class cuadrado extends rectangulo{
+    public cuadrado(double l){
+        super (l,l);
+    }
+}
+class triangulo implements Figuras{
+    double b,h;
+    public triangulo(double b, double h){
+        this.b=b;
+        this.h=h;
+    }
+    public double area(){
+        return (this.b*this.h/2);
+    }
+}
+class circulo implements Figuras{
+    double r;
+    public circulo(double r){
+        this.r=r;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI*this.r*this.r;
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,20 @@
+package proyecto;
+/**
+ * Created by alber on 17/09/2016.
+ */
+public class main {
+    public static void main(String[] args) {
+        Figuras[] ll = new Figuras[4];
+        ll[0]=new rectangulo(2,7);
+        ll[1]=new cuadrado(10);
+        ll[2]=new circulo(8);
+        ll[3]=new triangulo(6,9);
+        double suma=0;
+        for (Figuras f:ll){
+            suma=suma+f.area();
+
+        }
+        System.out.print(suma);
+    }
+}
+
